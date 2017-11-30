354f4b71a5e51184825aaab9559001fbfb5c0850
Daniel Vazquez <dvazquez21@berkeley.edu>
2017-11-29 22:21:01 -0800

    Users can sign in

--- foodbook/config/routes.rb
+++ foodbook/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
- 
-  devise_for :users
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root to: "home#index"
+	root to: "home#index"
+	get 'user/index'
+	get 'user/show'
+	get 'home/index'
+	devise_for :users
+	resources :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
